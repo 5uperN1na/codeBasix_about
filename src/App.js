f8c74325a36b8d656b60518bd6653229db24321a
Paula Suarez <Paula.Suarez@mainevent.com>
2020-11-04 17:07:17 -0600

    Adding styled components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
-import RotatingPawel from 'react-rotating-text';
+import styled from 'styled-components';
+import { keyframes } from 'styled-components';
+import slideInDown from 'react-animations/lib/slide-in-down';
+ 
 
-// let RotatingPawel = require('react-rotating-text');
+const Rotate = require('react-rotating-text');
 
-function App() {
+const AboutComponent: React.FC<IAboutComponentProps> = (props) => {
+
     return (
+
         <div>
             <div className="content">
                 <h1>Founders</h1>
@@ -23,9 +28,10 @@ function App() {
 
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuZGzBEJQnI8aAoFNVRmOp5fSOuViL9fxu3Q&usqp=CAU" />
             </div>
-            <div className="text">
-                <RotatingPawel style={{color: "orange"}}items={['is razor sharp.', 'is cautiously optimistic.', 'cannot lie to save his life.', 'is beyond loyal.', 'specifically honest.']} />
-            </div>
+            <Pawel>
+                <Rotate items={['is razor sharp.', 'is cautiously optimistic.', 'cannot lie to save his life.', 'is beyond loyal.', 'specifically honest.']} />
+            </Pawel>
+
 
             <div className="about-name">Nicole Pasquale</div>
 
@@ -33,9 +39,9 @@ function App() {
 
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuZGzBEJQnI8aAoFNVRmOp5fSOuViL9fxu3Q&usqp=CAU" />
             </div>
-            <div className="text">
-                <RotatingPawel style={{color: "#10A5F5"}} items={['is wicked smart.', 'is steady eddie.', 'is a giver.', 'is heart of gold.', 'is creative genius.']} />
-            </div>
+            <Nicole>
+                <Rotate items={['is wicked smart.', 'is steady eddie.', 'is a giver.', 'is heart of gold.', 'is creative genius.']} />
+            </Nicole>
 
             <div className="about-name">Paula Suarez</div>
 
@@ -43,21 +49,112 @@ function App() {
 
                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRuZGzBEJQnI8aAoFNVRmOp5fSOuViL9fxu3Q&usqp=CAU" />
             </div>
-            <div className="text">
-                <RotatingPawel style={{color: "#ef00a0"}} items={['is a life-long learner.', 'is passionately crazy.', 'is driven intensely.','is fearless.', 'is beyond relentless.']} />
-            </div>
+            <Paula>
+                <Rotate items={['is a life-long learner.', 'is passionately crazy.', 'is driven intensely.', 'is fearless.', 'is beyond relentless.']} />
+            </Paula>
 
+            <Container>
+                <Circle />
+            </Container>
 
+            <Rotater>foo</Rotater>
 
+ 
 
-        </div>
+            <AnimatedComponent>I am animated</AnimatedComponent>
 
-
-
-
-
+        </div >
 
     )
 }
 
-export default App;
+const breatheAnimation = keyframes`
+ 0% { height: 100px; width: 100px; }
+ 30% { height: 400px; width: 400px; opacity: 1 }
+ 40% { height: 405px; width: 405px; opacity: 0.3; }
+ 100% { height: 100px; width: 100px; opacity: 0.6; }
+`;
+
+const Circle = styled.div`
+ height: 100px;
+ width: 100px;
+ border-style: solid;
+ border-width: 5px;
+ border-radius: 50%;
+ border-color: black;
+ animation-name: ${breatheAnimation};
+ animation-duration: 8s;
+ animation-iteration-count: infinite;
+`;
+
+const Container = styled.div`
+ display: flex;
+ align-items: center;
+ justify-content: center;
+ flex-direction: column;
+ height: 450px;
+`;
+
+
+
+const Pawel = styled.div`
+color: orange;
+font-weight: bold;
+text-align: center;
+margin-right: 4rem;
+margin-bottom: 1rem;
+`;
+
+
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Rotater = styled.div`
+  display: inline-block;
+  animation: ${rotate} 2s linear infinite;
+  padding: 5px;
+  font-size: 3rem;
+`;
+
+
+ 
+
+
+const AnimatedComponent = styled.div`
+  animation: 1.5s ${keyframes(slideInDown)};
+`;
+
+
+
+
+
+
+
+
+
+
+
+const Nicole = styled.div`
+color: #10A5F5;
+font-weight: bold;
+text-align: center;
+margin-right: 4rem;
+margin-bottom: 1rem;
+`;
+
+const Paula = styled.div`
+color: #ef00a0;
+font-weight: bold;
+text-align: center;
+margin-right: 4rem;
+margin-bottom: 1rem;
+`;
+
+export interface IAboutComponentProps { }
+
+export default AboutComponent;
